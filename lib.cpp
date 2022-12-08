ea5b98599612f03dafa6a0126e5bf0e212d120bd
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-12-08 19:33:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "lib.h"
+
+bool lettera (char&a) {
+    bool risultato=false;
+
+    if ((a>64) && (a<91)){ //tutte lettere maiuscole
+        a=a+32;
+        risultato=true;
+    }
+    else if((a>96) && (a<123)) { //tutte lettere minuscole
+        a = a - 32;
+        risultato = true;
+    }
+
+    else{
+        risultato=false;
+
+
+    }
+    return risultato;
+}
 
